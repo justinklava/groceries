9297b511cea4aacfd2ae9c599d64c9a1e103021e
Justin Klava <email@justinklava.com>
2026-05-03 14:19:05 -0400

    some basic form functionality added

--- public/main.js
+++ public/main.js
@@ -2,3 +2,48 @@ const elmRoot = document.getElementById('root');
 const elmMain = document.getElementById('main');
 const elmHeader = document.getElementById('header');
 const elmFooter = document.getElementById('footer');
+const elmQuantity = document.getElementById('input-quantity');
+const elmItems = document.getElementById('div-items');
+const elmStore = document.getElementById('input-store');
+const elmOther = document.getElementById('span-otherstore');
+
+function buildStoreList(){
+    console.log('Entry: buildStoreList()');
+    //fetch list of stores and build list including 'other' with extra field to add store
+}
+
+function otherStore() {
+    console.log('Entry: otherStore()');
+    if(elmStore.value === 'Other') {
+        console.log('Other');
+        //display additional input for store name
+        elmOther.style.display = 'inline';
+        return;
+    }
+    elmOther.style.display = 'none';
+}
+
+function addItemsFields() {
+    console.log('Entry: addItemsFields()');
+    elmItems.replaceChildren();
+    for(let i=1; i<=elmQuantity.value; i++) {
+        //add row of input fields
+        const div = document.createElement('div');
+        div.id = 'items-div-' + i;
+        div.className = 'items-div';
+        elmItems.appendChild(div);
+        console.log(i);
+    }
+
+}
+
+function initialize() {
+    buildStoreList();
+    addItemsFields();
+    otherStore();
+}
+
+
+elmQuantity.addEventListener('input', addItemsFields);
+elmStore.addEventListener('input', otherStore);
+window.addEventListener('load', initialize);
--- src/routes/router.js
+++ src/routes/router.js
@@ -13,7 +13,9 @@ api.post('/insert', (req, res) => {
         return;
     }
     const test_response = {
-        success: true
+        success: true,
+        message: 'Successful test fetch',
+        body: req.body
     };
-    res.status(200).json('test');
+    res.status(200).json(test_response);
 });
