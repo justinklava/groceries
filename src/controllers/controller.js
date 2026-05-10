59a1babd2c89dd8a9f508820433d018349519cce
Justin Klava <email@justinklava.com>
2026-05-09 21:16:17 -0400

    moved all functionality from router into controller

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -3,16 +3,31 @@ import {fileURLToPath} from 'url';
 
 import dbq from '../config/db.js';
 
-export { Insert };
+export { postInsert, getStores };
 
 const currentDirectory = path.dirname(fileURLToPath(import.meta.url));
 
-async function Insert(obj) {
+async function postInsert(req, res) {
+    if (!req.body || Object.keys(req.body).length == 0) {
+        res.status(400).json({
+            error: true,
+            message: 'No body in request'
+        });
+        return;
+    }
+
     // get values from form object and process
     // call Insert functions with processed values
+    
+    const testResponse = {
+        success: true,
+        message: 'Successful test fetch',
+        body: req.body
+    };
+    res.status(200).json(testResponse);
 }
 
-async function InsertReceipt(receipt) {
+async function insertReceipt(receipt) {
     let storeID = receipt.storeID;
     if (storeID == 0) {
         storeID = await InsertStore(receipt.storeName);
@@ -26,13 +41,21 @@ async function InsertReceipt(receipt) {
     return data;
 }
 
-async function InsertPurchase(purchases) {
+async function insertPurchase(purchases) {
     const query = `INSERT INTO receipt ( ) VALUES ( )`;
     const data = await dbq(query);
     return data;
 }
 
-async function InsertStore(storeName) {
+async function getStores(req, res) {
+    const testResponse = {
+        storeName: ['Sample Store 1','Sample Store 2'],
+        storeID: [1,2]
+    }
+    res.status(200).json(testResponse);
+}
+
+async function insertStore(storeName) {
     // insert storeName and return id
     // if error return null
     return ;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,30 +1,9 @@
 import express from 'express';
-import { Insert } from '../controllers/controller.js';
+import { postInsert, getStores } from '../controllers/controller.js';
 
 const api = express.Router();
 export default api;
 
-api.get('/stores', (req, res) => {
-    const testResponse = {
-        storeName: ['Sample Store 1','Sample Store 2'],
-        storeID: [1,2]
-    }
-    res.status(200).json(testResponse);
-});
+api.get('/stores', await getStores);
 
-api.post('/insert', async (req, res) => {
-    if (!req.body || Object.keys(req.body).length == 0) {
-        res.status(400).json({
-            error: true,
-            message: 'No body in request'
-        });
-        return;
-    }
-    //const data = await Insert(req.body);
-    const testResponse = {
-        success: true,
-        message: 'Successful test fetch',
-        body: req.body
-    };
-    res.status(200).json(testResponse);
-});
+api.post('/insert', await postInsert);
